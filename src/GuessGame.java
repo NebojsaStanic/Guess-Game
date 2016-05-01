8d037b38a0d84a753c7c7115e1c4a4e71c558b2f
Nebojsa <nebojsa.stanic86@gmail.com>
2016-05-01 10:19:50 +0200

    Initial commit.

---
+++ src/GuessGame.java
@@ -0,0 +1,70 @@
+
+public class GuessGame {
+	
+	Player p1;
+	Player p2;
+	Player p3;
+	
+	public void startGame(){
+		p1 = new Player();
+		p2 = new Player();
+		p3 = new Player();
+		
+		int guessp1 = 0;
+		int guessp2 = 0;
+		int guessp3 = 0;
+		
+		boolean pl1isRight = false;
+		boolean pl2isRight = false;
+		boolean pl3isRight = false;
+		
+		int targetNumber = (int) (Math.random()*10);
+		System.out.println("I'm thinging of a number between 0 and 9...");
+		
+		while(true){
+			System.out.println("Number to guess is: " + targetNumber);
+			
+			p1.guess();
+			p2.guess();
+			p3.guess();
+			
+			guessp1 = p1.number;
+			System.out.println("Player one guessed: " + guessp1);
+			guessp2 = p2.number;
+			System.out.println("Player two guessed: " + guessp2);
+			guessp3 = p3.number;
+			System.out.println("Player three guessed: " + guessp3);
+			
+			if(guessp1 == targetNumber){
+				pl1isRight = true;
+			}
+			if(guessp2 == targetNumber){
+				pl2isRight = true;
+			}
+			if(guessp3 == targetNumber){
+				pl3isRight = true;
+			}
+			
+			if(pl1isRight){
+				System.out.println("We have a winner");
+				System.out.println("Player one got it right? " + pl1isRight);
+			}
+			else if(pl2isRight){
+				System.out.println("We have a winner");
+				System.out.println("Player two got it right? " + pl2isRight);
+			} 
+			else if(pl3isRight){
+				System.out.println("We have a winner");
+				System.out.println("Player three got it right? " + pl3isRight);
+			} 
+			else{
+				System.out.println("Game over");
+				System.out.println("Players will have to try again");
+			}
+			
+			
+			
+		}
+	}
+
+}
